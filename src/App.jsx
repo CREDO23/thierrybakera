6da0e4b07d4cca3808de1d1be5afce305d558dbc
CREDO23 <thierrybakera12@gmail.com>
2022-12-25 15:16:51 +0200

    feat : add layout/footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import Footer from './components/Footer'
 import './styles/css/index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <button className='btn'>
-        Envoyer
-      </button>
-    </div>
+  <Footer/>
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+export default function () {
+  return (
+    <footer>
+      <div className="hire-me-section">
+        <div>
+          <span>Have any project in mind ?</span>
+          <div></div>
+        </div>
+        <button>Hire me</button>
+      </div>
+      <div className="navigation-section">
+        <div className="media"></div>
+          <ul>
+            <li>About me</li>
+            <li>Projects</li>
+            <li>Services</li>
+            <li>Experiences</li>
+          </ul>
+      </div>
+    </footer>
+  );
+}
