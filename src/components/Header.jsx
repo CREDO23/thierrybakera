fbd9988568b040e6543359e1b843e592860e702c
CREDO23 <thierrybakera12@gmail.com>
2022-12-29 06:09:19 +0200

    feat : add header ( mobile )

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,20 @@
-import { useState } from "react";
-import logo from '../assets/thierry.png'
+
+import Navigation from "./Navigation";
 
 export default function () {
+ 
+  const navLikns = ["Home", "About me", "Services", "Experiences", "Projects"];
 
-    const [currentLink , setCurrentLink] = useState(0)
-    const navLikns = ['Home' , 'About me' , 'Services' , 'Experiences', 'Projects']
+ 
 
-    const handleActiveLink = (index) => {
-        console.log(currentLink)
-        setCurrentLink(index)
-    }
-    
   return (
     <header>
-        {/* <img src={logo} alt="thierry" srcset="" /> */}
-        <div className="logo">
-            <span className="first-letter">T</span>
-            <span >hierry</span>
-        </div>
-        <div>
-        <ul>
-          {
-            navLikns.map((link , index) => <li key={index} onClick={() => handleActiveLink(index)} className={currentLink == index ? 'active' : ''}>{link}</li>)
-          }
-      </ul>
-      <button>Hire me</button>
-        </div>
+      <div className="logo">
+        <span className="first-letter">T</span>
+        <span>hierry</span>
+      </div>
+       <Navigation/>
+        <button>Hire me</button>
     </header>
   );
 }
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,38 @@
+import { RxHamburgerMenu } from "react-icons/rx";
+import {MdCancelPresentation} from 'react-icons/md'
+import { useState } from "react";
+
+export default function () {
+
+    const [currentLink, setCurrentLink] = useState(0);
+    const [hamburgerOpen , setHamburgerOpen] = useState(false);
+
+
+  const navLikns = ["Home", "About me", "Services", "Experiences", "Projects"];
+
+  const handleActiveLink = (index) => {
+    console.log(currentLink);
+    setCurrentLink(index);
+  };
+
+  return (
+    <nav>
+      <div className="hamburger" onClick={() => setHamburgerOpen(!hamburgerOpen)}>
+      {
+        hamburgerOpen ? <MdCancelPresentation className={hamburgerOpen ? 'cancel-hamburger' : ''} /> :  <RxHamburgerMenu />
+      }
+      </div>
+      <ul className={hamburgerOpen ? 'mobile-menu' : ''}>
+        {navLikns.map((link, index) => (
+          <li
+            key={index}
+            onClick={() => handleActiveLink(index)}
+            className={currentLink == index ? "active" : ""}
+          >
+            {link}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
