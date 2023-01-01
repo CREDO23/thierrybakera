34443e32125907002a20e700d49d26f80ee7729e
CREDO23 <thierrybakera12@gmail.com>
2023-01-01 20:35:07 +0200

    feat : finish home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,44 @@
-import profil from '../assets/profil.png'
+import profil from "../assets/profil.png";
+import { IoIosArrowForward } from "react-icons/io";
+import { FaCheck } from "react-icons/fa";
 
-
-export default function(){
-    return <section className='home'>
-        <div className='catch-phrase'>
-            <h1>Hi , I am Thierry <br /> <span>SoftWare Developper</span></h1>    
-            <p>I Design and code beautifully Web Application and <br /> I love what I do</p>
-            <div className='buttons'>
-                <button className='cv'>
-                    Download CV
-                </button>
-                <button className='hire-me'>
-                    Hire me now
-                </button>
-            </div>
+export default function () {
+  return (
+    <section className="home">
+      <div className="catch-phrase">
+        <h1>
+          Hi , I am Thierry <br /> <span>Software Developper</span>
+        </h1>
+        <p>
+          I Design and code beautifully Web Application and <br /> I love what I
+          do
+        </p>
+        <div className="buttons">
+          <button className="cv">Download CV</button>
+          <button className="hire-me">Hire me now</button>
         </div>
-        <div className='profil'>
-            <img src={profil} alt="" srcset="" />
+      </div>
+      <div className="profil">
+        <img src={profil} alt="" srcset="" />
+        <div className="developper">
+          <div className="check">
+            <FaCheck />
+          </div>
+          <div>
+            <h4>Web Developper</h4>
+            <p>Creatives Websites for you </p>
+          </div>
         </div>
+        <div className="freelancer">
+          <div className="check">
+            <FaCheck />
+          </div>
+          <div>
+            <h4>Freelancer</h4>
+            <p>You can hire me now</p>
+          </div>
+        </div>
+      </div>
     </section>
+  );
 }
